85442b2888387ce36e6e4c2b082517ff12695f27
ju_lieca@yahoo.com <ju_lieca@yahoo.com>
2020-05-29 17:49:21 +0700

    Updates

---
+++ precache-manifest.fdc8563e138a7e3397b52f6414153c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d21b3f665be6a71fc6e0dc124a2f6a",
+    "url": "/julieca-iiswebtest/index.html"
+  },
+  {
+    "revision": "8b06a699127a6514ce5b",
+    "url": "/julieca-iiswebtest/static/css/main.68b590f0.chunk.css"
+  },
+  {
+    "revision": "3f867d3bf8c169b43de1",
+    "url": "/julieca-iiswebtest/static/js/2.cbdb5fdd.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/julieca-iiswebtest/static/js/2.cbdb5fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b06a699127a6514ce5b",
+    "url": "/julieca-iiswebtest/static/js/main.80eed157.chunk.js"
+  },
+  {
+    "revision": "52734ad916a00a31a5d2",
+    "url": "/julieca-iiswebtest/static/js/runtime-main.d972a0d8.js"
+  }
+]);
